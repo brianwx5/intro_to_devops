a028e83f3261c4b95a15468cb1f87b24f485d7fa
Marquis Chamberlain <Marquis_Chamberlain@comcast.com>
2019-04-17 16:02:35 -0400

    more

--- Module_F/setup_cluster/setup_cluster.sh
+++ Module_F/setup_cluster/setup_cluster.sh
@@ -37,6 +37,7 @@ docker cp ansible-playbook.yml cluster-master:/opt/ansible/launchpad/ansible-pla
 docker cp motd.j2 cluster-master:/opt/ansible/launchpad/motd.j2
 
 # time ansible-playbook /opt/ansible/launchpad/ansible-playbook.yml -i /opt/ansible/launchpad/host.txt
+# ansible -m setup hostname
 
 # for i in $(seq 1 1 20)
 # do
