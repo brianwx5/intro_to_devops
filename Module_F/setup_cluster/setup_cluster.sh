00d10d72439896195285c263eb2594f41316e90e
Marquis Chamberlain <Marquis_Chamberlain@comcast.com>
2019-04-17 12:46:38 -0400

    more

--- Module_F/setup_cluster/setup_cluster.sh
+++ Module_F/setup_cluster/setup_cluster.sh
@@ -20,14 +20,9 @@ docker run -i -t -d --name cluster-server cluster-server
 
 sleep 2
 
-#docker exec -it cluster-server /usr/sbin/sshd && echo "SSHD started"
-docker exec -it cluster-server /bin/sh /tmp/start_sshd.sh
-
 docker exec -it cluster-server netstat -anp
 
 docker inspect -f '{{range .NetworkSettings.Networks}}{{.IPAddress}}{{end}}' cluster-server
 
 ssh -o "StrictHostKeyChecking no" -i clusterkey root@$(docker inspect -f '{{range .NetworkSettings.Networks}}{{.IPAddress}}{{end}}' cluster-server)
 
-# docker exec -it cluster-server /bin/ash
-# docker exec -it cluster-server /usr/sbin/sshd
