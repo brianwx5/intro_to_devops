ccd35b3d5da8ee321b1a5fbb15edcf7c0f9a6afc
Marquis Chamberlain <Marquis_Chamberlain@comcast.com>
2019-04-17 13:04:49 -0400

    more

--- Module_F/setup_cluster/setup_cluster.sh
+++ Module_F/setup_cluster/setup_cluster.sh
@@ -16,8 +16,8 @@ docker build -t cluster-server --build-arg ssh_pub_key="$(cat ./clusterkey.pub)"
 
 sleep 2
 
-
-for i in `seq 0, 1, 20`; do
+for i in $(seq 1 1 20)
+do
 docker run -i -t -d --name cluster-server-${i} cluster-server
 docker exec -it cluster-server netstat -anp
 docker inspect -f '{{range .NetworkSettings.Networks}}{{.IPAddress}}{{end}}' cluster-server
