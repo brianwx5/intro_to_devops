5dc20421d9e92a695617704764f6e3b71e8deec3
Marquis Chamberlain <Marquis_Chamberlain@comcast.com>
2019-04-19 10:44:51 -0400

    more

--- Module_F/setup_cluster/linear_ps.sh
+++ Module_F/setup_cluster/linear_ps.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
-for ip_address in $(cat /tmp/host.txt)
+for ip_address in $(cat ./manage_list.txt)
 do
 echo "$ip_address"
 ssh -o "StrictHostKeyChecking no" -i clusterkey root@$ip_address 'ps -ef'
