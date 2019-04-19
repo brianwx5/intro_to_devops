96704efc5902a9a3da95e7349f66a964cf4779ff
Marquis Chamberlain <Marquis_Chamberlain@comcast.com>
2019-04-19 10:38:21 -0400

    more

--- Module_F/setup_cluster/multithread_config.py
+++ Module_F/setup_cluster/multithread_config.py
@@ -10,7 +10,7 @@ def worker():
         cmd_list.append("ps -ef")
         print(cmd_list)
         #output = subprocess.Popen(["ssh", "-o", " StrictHostKeyChecking no", "-i",  "clusterkey", "root@172.17.0.3", "ps -ef"], stdout=subprocess.PIPE, stderr=subprocess.STDOUT)
-        output = subprocess.Popen([cmd_list], stdout=subprocess.PIPE, stderr=subprocess.STDOUT)
+        output = subprocess.Popen(cmd_list, stdout=subprocess.PIPE, stderr=subprocess.STDOUT)
         so, er = output.communicate()
         cmd_output = str(so.decode("utf-8"))
         print(cmd_output)
